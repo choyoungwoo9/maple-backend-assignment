40a5b2e6bf23cdb3d4ab35cd2a28070a9a249360
조영우 <choyoungwoo9@naver.com>
2025-05-16 14:10:06 +0900

    chore: [auth service]프리티어 포매팅 수정
    - 인덴트: 공백 4개 -> 탭 1개

--- auth-service/src/app.controller.ts
+++ auth-service/src/app.controller.ts
@@ -7,25 +7,25 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 @ApiTags('auth')
 @Controller('auth')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Post('users/signup')
-  @ApiOperation({ summary: '회원가입' })
-  @ApiResponse({
-    status: 201,
-    description: '회원가입 성공 및 액세스 토큰 반환',
-    type: SignupUserResponseDto,
-  })
-  @ApiResponse({ status: 400, description: '요청 Body 형식 오류' })
-  @ApiResponse({ status: 401, description: 'id 중복' })
-  async signupUser(
-    @Body() dto: SignupUserRequestDto,
-  ): Promise<SignupUserResponseDto> {
-    const { id, password } = dto;
-    const accessToken = await this.appService.signupUser(id, password);
-    const response: SignupUserResponseDto = {
-      accessToken: accessToken,
-    };
-    return response;
-  }
+	@Post('users/signup')
+	@ApiOperation({ summary: '회원가입' })
+	@ApiResponse({
+		status: 201,
+		description: '회원가입 성공 및 액세스 토큰 반환',
+		type: SignupUserResponseDto,
+	})
+	@ApiResponse({ status: 400, description: '요청 Body 형식 오류' })
+	@ApiResponse({ status: 401, description: 'id 중복' })
+	async signupUser(
+		@Body() dto: SignupUserRequestDto,
+	): Promise<SignupUserResponseDto> {
+		const { id, password } = dto;
+		const accessToken = await this.appService.signupUser(id, password);
+		const response: SignupUserResponseDto = {
+			accessToken: accessToken,
+		};
+		return response;
+	}
 }
--- auth-service/src/app.service.ts
+++ auth-service/src/app.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  async signupUser(id: string, password: string): Promise<string> {
-    console.log(`신규 회원가입: ${id}`);
-    return 'access token';
-  }
+	async signupUser(id: string, password: string): Promise<string> {
+		console.log(`신규 회원가입: ${id}`);
+		return 'access token';
+	}
 }
