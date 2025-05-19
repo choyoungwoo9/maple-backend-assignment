cc05df6caf44e508b9c8c42c177646f1d8246b8c
조영우 <choyoungwoo9@naver.com>
2025-05-19 12:54:00 +0900

    feat: [auth server] 애플리케이션 시작 시 루트 관리자 생성

--- auth-server/server/src/service/auth.service.ts
+++ auth-server/server/src/service/auth.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { AccountDomain, Role } from '../domain/account.domain';
 import { AccountRepository } from 'src/repository/account.repository';
-import {
-	AuthServiceException,
-	AuthServiceUnexpectedException,
-	AuthServiceIdDuplicatedException,
-} from './exception/auth.service.exception';
+import { AuthServiceIdDuplicatedException } from './exception/auth.service.exception';
+import { WrapWith } from 'src/common/decorator/wrap-with.decorator';
+import { AuthServiceExceptionWrapStrategy } from './exception/auth.service.exception.wrap-strategy';
 
+@WrapWith(AuthServiceExceptionWrapStrategy)
 @Injectable()
-export class AuthService {
+export class AuthService implements OnApplicationBootstrap {
 	constructor(private readonly accountRepository: AccountRepository) {}
+	async onApplicationBootstrap() {
+		const signupRootAdmin = async () => {
+			const rootId = process.env.AUTH_SERVER_ROOT_ADMIN_ID;
+			const rootPw = process.env.AUTH_SERVER_ROOT_ADMIN_PW;
+			if (!rootId || !rootPw) return;
+
+			const exists = await this.accountRepository.isExistById(rootId);
+			if (!exists) {
+				const root = await AccountDomain.create({
+					id: rootId,
+					password: rootPw,
+					role: Role.Admin,
+				});
+				await this.accountRepository.create(root);
+			}
+		};
+		await signupRootAdmin();
+	}
+
 	async signupUser({
 		id,
 		password,
@@ -17,22 +35,15 @@ export class AuthService {
 		id: string;
 		password: string;
 	}): Promise<void> {
-		try {
-			const isExist = await this.accountRepository.isExistById(id);
-			if (isExist) {
-				throw new AuthServiceIdDuplicatedException({});
-			}
-			const user = await AccountDomain.create({
-				id,
-				password,
-				role: Role.User,
-			});
-			await this.accountRepository.create(user);
-		} catch (error) {
-			if (error instanceof AuthServiceException) {
-				throw error;
-			}
-			throw new AuthServiceUnexpectedException({ cause: error });
+		const isExist = await this.accountRepository.isExistById(id);
+		if (isExist) {
+			throw new AuthServiceIdDuplicatedException({});
 		}
+		const user = await AccountDomain.create({
+			id,
+			password,
+			role: Role.User,
+		});
+		await this.accountRepository.create(user);
 	}
 }
